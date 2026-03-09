3b2598a3381f35b750ca280b5eb6a375ce58cd49
Fehings <frances.hutchings@newcastle.ac.uk>
2026-03-09 15:45:15 +0000

    Added docstrings to functions

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -2,6 +2,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def read_json_to_dataframe(input_file):
+    """
+    Read the data from a JSON file into a Pandas dataframe.
+    Clean the data by removing any rows where 'duration' value is missing.
+
+    Args:
+        input_file (file or str): The file object or path to the JSON file.
+
+    Returns:
+        eva_df (pd.DataFrame): The cleaned data as a dataframe structure.
+    """
     print(f'Reading JSON file {input_file}')
     # Read the data from a JSON file into a Pandas dataframe
     eva_df = pd.read_json(input_file, convert_dates=['date'], encoding='ascii')
@@ -11,11 +21,31 @@ def read_json_to_dataframe(input_file):
     return eva_df
 
 def write_dataframe_to_csv(df, output_file):
+    """
+    Write the dataframe to a CSV file.
+
+    Args:
+        df (pd.DataFrame): The input dataframe.
+        output_file (file or str): The file object or path to the output CSV file.
+
+    Returns:
+        None
+    """
     print(f'Saving to CSV file {output_file}')
     # Save dataframe to CSV file for later analysis
     df.to_csv(output_file, index=False, encoding='utf-8')
 
 def plot_cumulative_time_in_space(eva_df, graph_file):
+    """
+    Plot cumulative time spent in space over years and save the graph to a file.
+
+    Args:
+        eva_df (pd.DataFrame): The input dataframe.
+        graph_file (file or str): The file object or path to the output graph file.
+
+    Returns:
+        None
+    """
     # Plot cumulative time spent in space over years
     print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
     eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
